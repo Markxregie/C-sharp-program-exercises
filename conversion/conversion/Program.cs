f2264454f50a0629488539bebe640f9c1087b51a
Mark Regie A. Magtangob <110184282+Markxregie@users.noreply.github.com>
2024-04-27 19:20:40 +0800

    Add files via upload

---
+++ conversion/conversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string x = "6.7";
+            string y = "10.23";
+
+            float num1 = Convert.ToSingle(x);
+            float num2 = Convert.ToSingle(y);
+
+            Console.WriteLine(x + y);
+            Console.WriteLine(num1 + num2);
+;           
+
+        }
+    }
+}
---
+++ methods/methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace methods
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(add(5, 2,3));
+        }
+       static int add (int num1, int num2)
+        {
+            return num1 + num2;
+        }
+       static int add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+    }
+}
