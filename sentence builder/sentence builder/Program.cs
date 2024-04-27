1bc81c7662ce80bae1d808f5c70cea5de8dd1615
Mark Regie A. Magtangob <110184282+Markxregie@users.noreply.github.com>
2024-04-27 19:16:58 +0800

    Add files via upload

---
+++ sentence builder/sentence builder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sentence_builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mark";
+            int age = 18;
+            float gpa = 99.9f;
+            char sex = 'M';
+
+            Console.WriteLine("Hi my name is " + name);
+            Console.WriteLine("I'am " + age + " years old");
+            Console.WriteLine("My GPA is " + gpa);
+            Console.WriteLine("My Sex is " + sex);
+
+
+        }
+    }
+}
---
+++ simulation/simulation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+class student
+{
+    public void introduceSelf(string fullname, string course, int year, string section)
+    {
+        Console.WriteLine("Fullname: " + fullname);
+        Console.WriteLine("Course: " +  course);
+        Console.WriteLine("Year: " + year);
+        Console.WriteLine("Section: " + section);
+    }
+
+
+    public void grade(int grade)
+    {
+        Console.WriteLine("Average: "+ grade);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("With honors");
+        }
+        else if (grade >= 95)
+        {
+            Console.WriteLine("With High Honor");
+        }
+        else if (grade >= 98)
+        {
+            Console.WriteLine("With Highest Honor");
+        }
+        else if (grade >= 75)
+        {
+            Console.WriteLine("Passed");
+        }
+
+        else 
+        {
+            Console.WriteLine("Failed");
+        }
+    }
+
+}
